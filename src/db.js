b105e412967cfb406ce0e2f7325afd9c7b1211cf
redim <redim@redim.com>
2024-10-06 19:57:13 +0800

    update crud mongodb

--- src/db.js
+++ src/db.js
@@ -1,32 +1,58 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
 
 class MongoDb {
   constructor() {
-    this.db = this.connectDb();
+    this.client = null;
+    this.employees = null;
   }
 
   async connectDb() {
     try {
-      const url = process.env.MONGODB_URL;
-      const client = new MongoClient(url);
+      if (this.employees) return;
+
+      const client = new MongoClient(process.env.MONGODB_URL);
       await client.connect();
       const database = client.db(process.env.DB_NAME);
-
-      console.log("Connected to MongoDB server");
-      return database;
+      this.employees = database.collection("employees");
+      console.log("Database has been connected");
     } catch (error) {
       console.error("Failed to connect to MongoDB server", error);
     }
   }
 
-  async getAllEmployees() {
-    return await this.db;
+  async getAllEmployees({ department, sex, employeeType }) {
+    console.log("Getting Employees....");
+    console.log(department, sex, employeeType);
+    if (department || sex || employeeType) {
+      console.log("Filtering Employees....");
+      return this.employees
+        .find({
+          department: department | "",
+          sex: sex | "",
+          employeeType: employeeType | "",
+        })
+        .toArray();
+    }
+    return this.employees.find().toArray();
+  }
+
+  async getEmployeeById(id) {
+    console.log("Getting Employee by ID....");
+    return this.employees.findOne({ _id: new ObjectId(id) });
+  }
+
+  async AddEmployee(newEmployee) {
+    try {
+      await this.employees.insertOne(newEmployee);
+      console.log("Employee added successfully");
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
-const MongoDbClient = new MongoDb();
-await new Promise((resolve) => setTimeout(resolve, 1000));
-
-console.log(MongoDbClient.getAllEmployees());
+module.exports = {
+  MongoDb,
+};
