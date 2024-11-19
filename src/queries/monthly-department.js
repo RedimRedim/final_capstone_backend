d63d5e2e25332acff2ed2ba4c2e63ed84ffb8ffb
redim <redim@redim.com>
2024-11-19 22:33:50 +0700

    re-structure mongodb query

--- src/queries/monthly-department.js
+++ src/queries/monthly-department.js
@@ -12,13 +12,31 @@ const getMonthlyDepartmentQuery = [
       absentDeduction: {
         $sum: "$absentDeduction",
       },
+      totalEmployeesReleased: { $sum: 1 },
+      totalResign: { $sum: { $cond: [{ $eq: ["$isResign", true] }, 1, 0] } },
     },
   },
+
+  {
+    $addFields: {
+      resignRatioRate: {
+        $cond: [
+          { $eq: ["$totalEmployeesReleased", 0] },
+          0,
+          { $divide: ["$totalResign", "$totalEmployeesReleased"] },
+        ],
+      },
+    },
+  },
+
   {
     $project: {
       month: "$_id.month",
       year: "$_id.year",
       department: "$_id.department",
+      totalEmployees: "$totalEmployeesReleased",
+      totalResign: "$totalResign",
+      resignRatioRate: "$resignRatioRate",
       lateMinutes: "$lateMinutes",
       absentDays: "$absentDays",
       lateDeduction: "$lateDeduction",
