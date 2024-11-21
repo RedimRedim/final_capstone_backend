0ee2868a30918ecf5349248418f1579c4964ef53
redim <redim@redim.com>
2024-11-21 20:57:34 +0700

    revert monthly-salary.js

--- src/queries/monthly-salary.js
+++ src/queries/monthly-salary.js
@@ -4,83 +4,26 @@ const queryMonthlySalary = [
       _id: {
         month: "$month",
         year: "$year",
-        department: "$department",
       },
-      lateMinutes: {
-        $sum: "$late",
+      totalSalary: {
+        $sum: "$totalReleasedSalary",
       },
-      absentDays: {
-        $sum: "$absent",
-      },
-      lateDeduction: {
-        $sum: "$lateDeduction",
-      },
-      absentDeduction: {
-        $sum: "$absentDeduction",
-      },
-      totalEmployeesReleased: {
-        $sum: 1,
-      },
+      totalEmployeesReleased: { $sum: 1 },
       totalResign: {
         $sum: {
-          $cond: [
-            {
-              $and: [
-                {
-                  $eq: ["$isResign", true],
-                },
-                {
-                  $eq: [
-                    {
-                      $month: "$resignDate",
-                    },
-                    "$month",
-                  ],
-                },
-                {
-                  $eq: [
-                    {
-                      $year: "$resignDate",
-                    },
-                    "$year",
-                  ],
-                },
-              ],
-            },
-            1,
-            0,
-          ],
+          $cond: [{ $eq: ["$isResign", true] }, 1, 0],
         },
       },
     },
   },
-  {
-    $addFields: {
-      resignRatioRate: {
-        $cond: [
-          {
-            $eq: ["$totalEmployeesReleased", 0],
-          },
-          0,
-          {
-            $divide: ["$totalResign", "$totalEmployeesReleased"],
-          },
-        ],
-      },
-    },
-  },
+
   {
     $project: {
-      month: "$_id.month",
       year: "$_id.year",
-      department: "$_id.department",
-      totalEmployees: "$totalEmployeesReleased",
+      month: "$_id.month",
+      totalSalary: "$totalSalary",
+      totalEmployeesReleased: "$totalEmployeesReleased",
       totalResign: "$totalResign",
-      resignRatioRate: "$resignRatioRate",
-      lateMinutes: "$lateMinutes",
-      absentDays: "$absentDays",
-      lateDeduction: "$lateDeduction",
-      absentDeduction: "$absentDeduction",
       _id: 0,
     },
   },
